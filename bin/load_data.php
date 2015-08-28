35a006ec2d1e0f320737819013bbc6cb12943203
Maurice Prosper <maurice.prosper@ttu.edu>
2015-08-28 00:24:56 -0500

    Binary file calls static methods as it should

--- bin/load_data.php
+++ bin/load_data.php
@@ -1,17 +1,32 @@
 <?php
 
-LOAD DATA
-	LOW_PRIORITY
-	LOCAL
-	INFILE '%s'
-	%s
-	INTO TABLE %s
-	FIELDS
-		TERMINATED BY ''
-		ENCLOSED BY '"'
-		ESCAPED BY '\\'
-	LINES
-		STARTING BY ''
-		TERMINATED BY '\n'
-	IGNORE 0 LINES
-	(%s);
+require 'vendor/autoload.php';
+require 'generated-conf/config.php'; // Propel's autoloader
+
+function fail($message) {
+	fwrite(STDERR, $message . PHP_EOL);
+	exit(1);
+}
+
+$opt = getopt('n:s:');
+
+if(!isset($opt['n']) && !isset($opt['s']))
+	fail('Missing reqiured option');
+
+$namespace = $opt['n'];
+$maps = is_array($opt['s']) ? $opt['s'] : array($opt['s']);
+
+foreach($maps as $map) {
+	$class = $namespace .'\\'. $map;
+	$classMap = $namespace .'\\Map\\'. $map . 'TableMap';
+	
+	if(!is_callable([$class, 'load']))
+		fail(sprintf('TableMap %s can not be loaded [-s %s]', $class, $map));
+	
+	try {
+		printf("Loading %s.\n", $map);
+		$class::load();
+	} catch (\Exception $e) {
+		fail($e->getMessage());
+	}
+}
